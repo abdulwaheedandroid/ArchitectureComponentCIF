34c33c113524bccf93fc93d6131a707761dabe9e
Abdul Waheed <bawan@abacusoft.com>
2019-11-25 19:00:36 +0500

    Part Seven implementation 1) Add note activity 2) Added new drawables 3) Added menu 4) Work wor navigation and saving data to Db

---
+++ app/src/main/java/com/abdul_waheed/architecturecomponentcodinginflow/AddNoteActivity.java
@@ -0,0 +1,79 @@
+package com.abdul_waheed.architecturecomponentcodinginflow;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    public static final String EXTRA_TITLE = "extra_title";
+    public static final String EXTRA_DESCRIPTION = "extra_description";
+    public static final String EXTRA_PRIORITY = "extra_priority";
+
+
+    private EditText etTitle, etDescription;
+    private NumberPicker numberPicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+
+        etTitle = findViewById(R.id.et_title);
+        etDescription = findViewById(R.id.et_description);
+        numberPicker = findViewById(R.id.numer_picker_priority);
+
+        numberPicker.setMinValue(1);
+        numberPicker.setMaxValue(10);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        setTitle("Add Note");
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_note:
+                saveNote();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void saveNote() {
+        String title = etTitle.getText().toString();
+        String desc = etDescription.getText().toString();
+        int priority = numberPicker.getValue();
+        if (title.trim().isEmpty() || desc.trim().isEmpty()) {
+            Toast.makeText(this, "Please insert a title and description", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_TITLE, title);
+        intent.putExtra(EXTRA_DESCRIPTION, desc);
+        intent.putExtra(EXTRA_PRIORITY, priority);
+
+        setResult(RESULT_OK, intent);
+
+        finish();
+
+    }
+}
